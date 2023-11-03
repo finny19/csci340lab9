f2d74a2ff6190d9273f8f10fbca42345863ff36c
Fin Lockert <fin@Fins-MBP.students.hendrix.local>
2023-11-02 23:23:32 -0500

    half through tutorial

---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+namespace SillyGooseSchool.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public int Age { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
