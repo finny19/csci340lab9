07b040e01671ebdb426187c3972afc65cd479318
Fin Lockert <fin@Fins-MBP.students.hendrix.local>
2023-11-03 00:35:57 -0500

    read related data

---
+++ Pages/Courses/Create.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SillyGooseSchool.Data;
+using SillyGooseSchool.Models;
+
+namespace SillyGooseSchool.Pages.Courses
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SillyGooseSchool.Data.SchoolContext _context;
+
+        public CreateModel(SillyGooseSchool.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID");
+            return Page();
+        }
+
+        [BindProperty]
+        public Course Course { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Courses.Add(Course);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
